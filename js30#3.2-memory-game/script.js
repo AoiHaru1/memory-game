af2f04e986012d00b8677036534a4078c9e7ea30
Kirill Kolupaev <snakeinwater1@gmail.com>
2021-12-26 15:42:55 +0300

    feat: add random picture set on start

--- js30#3.2-memory-game/script.js
+++ js30#3.2-memory-game/script.js
@@ -1,4 +1,10 @@
 const fieldCell = document.querySelectorAll('.field__cell');
+const cellPicture = document.querySelectorAll('.hidden-picture');
+
+
+const pictures = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+const cellsIndex = [...fieldCell].map((c, i) => i)
+
 
 // add card flip animation and event listener on click for it
 
@@ -8,18 +14,24 @@ fieldCell.forEach(x => {
 })
 
 
+//randomize pictures and cells order
 
+const randomize = arr => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const randInd = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[randInd]] = [arr[randInd], arr[i]];
+    }
+    return arr
+}
 
+const setPictureIntoCells = () => {
+  const randomPictures = randomize(pictures);
+  const randomCellsIndex = randomize(cellsIndex);
 
+  for (let i = 0, j = 0; i < randomPictures.length; i+=1, j+=2) {
+    cellPicture[randomCellsIndex[j]].src = `./img/${randomPictures[i]}.png`
+    cellPicture[randomCellsIndex[j + 1]].src = `./img/${randomPictures[i]}.png`
+  }
+}
 
-// let arr = [11, 22, 33, 44, 55, 66, 77, 88 ,99]
-
-// function shuffleArray(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [array[i], array[j]] = [array[j], array[i]];
-//     }
-//     return array
-// }
-
-// console.log(shuffleArray(arr))
+setPictureIntoCells()
