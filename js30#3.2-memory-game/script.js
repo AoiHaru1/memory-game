724cf9e582dad2db9d85ca8365c53bd9d0265732
Kirill Kolupaev <snakeinwater1@gmail.com>
2021-12-27 16:58:12 +0300

    feat: add timer and list of record in local storage

--- js30#3.2-memory-game/script.js
+++ js30#3.2-memory-game/script.js
@@ -1,8 +1,11 @@
 const fieldCell = document.querySelectorAll('.field__cell');
 const cellPicture = document.querySelectorAll('.hidden-picture');
+const timer = document.querySelector('.timer');
+const timerSeconds = document.querySelector('.timer__seconds');
+const timerMinutes = document.querySelector('.timer__minutes');
 
 
-const pictures = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+const pictures = ['01'];
 const cellsIndex = [...fieldCell].map((c, i) => i);
 
 let pictureCheck = [];
@@ -16,6 +19,9 @@ fieldCell.forEach(x => {
     if (maxSizeOfClickedPictures === false) {
       x.style.transform = 'rotateY(360deg)'
     }
+    if (activeInterval === false) {
+      runTimer()
+    }
   });
 })
 
@@ -23,11 +29,11 @@ fieldCell.forEach(x => {
 //randomize pictures and cells order
 
 const randomize = arr => {
-    for (let i = arr.length - 1; i > 0; i--) {
-        const randInd = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[randInd]] = [arr[randInd], arr[i]];
-    }
-    return arr
+  for (let i = arr.length - 1; i > 0; i--) {
+    const randInd = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[randInd]] = [arr[randInd], arr[i]];
+  }
+  return arr
 }
 
 // pictures set into cells
@@ -36,7 +42,7 @@ const setPictureIntoCells = () => {
   const randomPictures = randomize(pictures);
   const randomCellsIndex = randomize(cellsIndex);
 
-  for (let i = 0, j = 0; i < randomPictures.length; i+=1, j+=2) {
+  for (let i = 0, j = 0; i < randomPictures.length; i += 1, j += 2) {
     cellPicture[randomCellsIndex[j]].src = `./img/${randomPictures[i]}.png`;
     cellPicture[randomCellsIndex[j + 1]].src = `./img/${randomPictures[i]}.png`;
   }
@@ -49,12 +55,11 @@ setPictureIntoCells()
 const setSuccesState = (name) => {
   fieldCell.forEach(x => {
     if (x.childNodes[0].src.includes(name)) {
-      console.log("OPPA")
       x.classList.add('succes')
     }
   })
 }
- 
+
 // reset functions
 
 const clearClickedState = () => {
@@ -69,7 +74,7 @@ const clearWrongCombination = () => {
   })
 }
 
-const CheckerVariablesReset = () => {
+const VariablesCheckerReset = () => {
   maxSizeOfClickedPictures = false;
   pictureCheck = [];
 }
@@ -82,12 +87,12 @@ const PictureCheckOnSecondReveal = () => {
     if (pictureCheck[0] !== pictureCheck[1]) {
       setTimeout(() => {
         clearWrongCombination()
-        CheckerVariablesReset()
+        VariablesCheckerReset()
         clearClickedState();
       }, 1000)
     } else {
       setSuccesState(pictureCheck[0])
-      CheckerVariablesReset()
+      VariablesCheckerReset()
     }
   }
 }
@@ -106,3 +111,61 @@ fieldCell.forEach(x => {
 })
 
 
+// timer and local storage implement
+
+let timeInterval;
+let activeInterval = false;
+
+const getZero = x => x < 10 ? `0${x}` : x;
+
+const completeChecker = () => {
+  for (let i = 0; i < fieldCell.length; i++) {
+    if (!fieldCell[i].classList.contains('succes')) {
+      return false
+    }
+  }
+  return true
+}
+
+const sortLocalStorage = () => {
+  const getMinutes = x => +x.slice(0, 2);
+  const getSeconds = x => +x.slice(-2);
+  const storageValues = Object.values(localStorage);
+  const sortedStorageValues = storageValues.sort((a, b) => getMinutes(a) - getMinutes(b)).sort((a, b) => {
+    if (getMinutes(a) === getMinutes(b)) {
+      return getSeconds(a) - getSeconds(b)
+    }
+  })
+  for (let i = 0; i < localStorage.length; i++) {
+    localStorage[i + 1] = sortedStorageValues[i]
+  }
+}
+
+const setTimeToLocalStorage = () => {
+  let localStorageKeys = Object.keys(localStorage)
+  if (+`${localStorageKeys.length + 1}` !== 11) {
+    localStorage.setItem(`${localStorageKeys.length + 1}`, `${timerMinutes.innerHTML}:${timerSeconds.innerHTML}`)
+  } else {
+    localStorage.setItem(`10`, `${timerMinutes.innerHTML}:${timerSeconds.innerHTML}`)
+  }
+}
+
+const timerTick = () => {
+  timerSeconds.innerHTML = getZero(+timerSeconds.innerHTML + 1)
+  if (+timerSeconds.innerHTML === 60) {
+    timerMinutes.innerHTML = getZero(+timerMinutes.innerHTML + 1)
+    timerSeconds.innerHTML = "00"
+  }
+  if (completeChecker()) {
+    setTimeToLocalStorage()
+    clearInterval(timeInterval)
+    sortLocalStorage()
+  }
+}
+
+const runTimer = () => {
+  activeInterval = true;
+  timeInterval = setInterval(timerTick, 1000)
+}
+
+console.log(Object.keys(localStorage))
